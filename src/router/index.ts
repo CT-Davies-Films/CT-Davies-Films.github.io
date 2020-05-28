efbe2a4a9194ced23fef2003da2c16bb68d39b6c
Alex Scott <alex@codetheoristdevelopments.co.uk>
2020-05-28 10:41:30 +0100

    Stub gallery, portfolio & contact pages

--- src/router/index.ts
+++ src/router/index.ts
@@ -18,6 +18,33 @@ const routes: Array<RouteConfig> = [
     // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/About.vue")
+  },
+  {
+    path: "/gallery",
+    name: "Gallery",
+    // route level code-splitting
+    // this generates a separate chunk (gallery.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "gallery" */ "../views/Gallery.vue")
+  },
+  {
+    path: "/portfolio",
+    name: "Portfolio",
+    // route level code-splitting
+    // this generates a separate chunk (portfolio.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "portfolio" */ "../views/Portfolio.vue")
+  },
+  {
+    path: "/contact",
+    name: "Contact",
+    // route level code-splitting
+    // this generates a separate chunk (contact.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "contact" */ "../views/Contact.vue")
   }
 ];
 
---
+++ src/views/Gallery.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="about" id="gallery-page">
+    <h1>This is a gallery page</h1>
+  </div>
+</template>
